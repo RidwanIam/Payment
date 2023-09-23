038321039b00279c546e308621a0b7cd300afa7c
Ridwan Illham <137150785+RidwanIam@users.noreply.github.com>
2023-09-23 13:48:45 +0700

    Study kasus Warung Kopi
    Code Upload

---
+++ OPP Pertemuan 4/payment/src/payment/Order.java
@@ -0,0 +1,24 @@
+package payment;
+public class Order {
+    String name,type,sugar ;
+    int qty;
+        public Order(String name, String type, String sugar, int qty) {
+            this.name = name;
+            this.type = type;
+            this.sugar = sugar;
+            this.qty = qty;
+        }
+        public String getName() {
+            return name;
+        }
+        public String getType() {
+            return type;
+        }
+        public String getSugar() {
+            return sugar;
+        }
+        public int getQty() {
+            return qty;
+        }
+        
+    }
